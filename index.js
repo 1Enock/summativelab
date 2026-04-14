0fcdc374871b026db9e2d2a20d07d4b691bda6e3
Enock <enock.kibet@student.moringaschool.com>
2026-04-14 14:30:44 +0300

    Created the other html files on the services offered

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.getElementById("firstcard").onclick = function() {
     window.location.href = "render.html";
 };
+document.getElementById("secondcard").onclick = function() {
+    window.location.href = "gamedev.html";
+};
+document.getElementById("thirdcard").onclick = function() {
+    window.location.href = "webdev.html";
+};
