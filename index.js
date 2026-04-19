41b3f1fc844f27d9cc72feef9b9845469716281c
Enock <enock.kibet@student.moringaschool.com>
2026-04-19 23:08:14 +0300

    Boosted the card's functionality

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-// const firstcard = document.getElementById("firstcard");
-// const secondcard = document.getElementById("secondcard");
-// const thirdcard = document.getElementById("thirdcard");
+const firstcard = document.getElementById("firstcard");
+const secondcard = document.getElementById("secondcard");
+const thirdcard = document.getElementById("thirdcard");
 
-// if (firstcard) firstcard.onclick = function() { window.location.href = "render.html"; };
-// if (secondcard) secondcard.onclick = function() { window.location.href = "gamedev.html"; };
-// if (thirdcard) thirdcard.onclick = function() { window.location.href = "webdev.html"; };
+if (firstcard) firstcard.onclick = function() { window.location.href = "render.html"; };
+if (secondcard) secondcard.onclick = function() { window.location.href = "gamedev.html"; };
+if (thirdcard) thirdcard.onclick = function() { window.location.href = "webdev.html"; };
+
 export function wireCardNavigation(
   doc = document,
   navigate = (url) => {
